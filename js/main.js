4a1687a822e9afd9e58496a192e1cfcf61bc8d72
fuckdantez <max2afonichev@gmail.com>
2020-06-06 20:55:30 +0300

    Fisrt upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+/*   $(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left slick-arrow" alt="" style="" src="images/arrows-left.svg">',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right slick-arrow" alt="" style="" src="images/arrows-right.svg">',
+    asNavFor: ".name",
+  });
+  $(".slider-dotshead").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".name",
+  }); */
+});
